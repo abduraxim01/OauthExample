ab39c84c1995fdc9ab0b987c2498b49133e00f3e
Abduraxim <150508362+abduraxim01@users.noreply.github.com>
2025-04-23 11:24:11 +0500

    Add files via upload

---
+++ src/test/java/uz/abduraxim/OauthExample/OauthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.abduraxim.OauthExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
