dafd1073bbed0b5d0b2136571472b1cc7aa016f2
Asped <aspedasped41@gmail.com>
2025-04-23 14:07:42 +0500

    without google credentials

---
+++ src/main/java/uz/abduraxim/OauthExample/controller/MainController.java
@@ -0,0 +1,15 @@
+package uz.abduraxim.OauthExample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/main")
+public class MainController {
+
+    @GetMapping(value = "/maqsad")
+    public String get(){
+        return "O'zbekistonni birgalikda yuksaltiramiz.";
+    }
+}
